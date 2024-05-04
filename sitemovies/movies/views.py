f519e8fcf25ab0b2821c6d53baf783d6e3b89297
Mariia <mariiaboiko.it@gmail.com>
2024-05-04 16:32:24 +0300

    Initial Commit

--- sitemovies/movies/views.py
+++ sitemovies/movies/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+movies = [
+     {
+         "title": "Movie 1",
+         "duration": "120 min",
+         "year": 2023,
+         "relevance": True
+     },
+     {
+         "title": "Movie 2",
+         "duration": "90 min",
+         "year": 2021,
+         "relevance": False
+     },
+     {
+         "title": "Movie 3",
+         "duration": "150 min",
+         "year": 2024,
+         "relevance": True
+     }
+]
+
+def get_movies(request):
+    data = {"movies": movies}
+    return render(request, 'movies/movies.html', context=data)
